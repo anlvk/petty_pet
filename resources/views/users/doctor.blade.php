d4960e548afaee3b04b7c2877d4e3ffb88f7aca3
Hanna Liauchuk <anne.levchuk@gmail.com>
2025-12-16 22:55:54 +0300

    Update UserController and Blade view to implement session flash output

--- resources/views/users/doctor.blade.php
+++ resources/views/users/doctor.blade.php
@@ -1,5 +1,19 @@
 <h5>SET DOCTOR'S SPECIALIZATION</h5>
 
+{{-- Check if a 'success' flash message exists --}}
+@if(session()->has('success'))
+    <div class="alert alert-success" style="background-color: green; color: white;">
+        {{ session('success') }}
+    </div>
+@endif
+
+{{-- Check for an 'error' or 'danger' message --}}
+@if(session()->has('error'))
+    <div class="alert alert-danger">
+        {{ session('error') }}
+    </div>
+@endif
+
 <form action="/users/doctor" method="POST" id="doctorForm">
     @csrf
     <input type="hidden" name="userID" value="{{ $userID }}">
